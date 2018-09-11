009e5a6eccc165fa56d56ab35a4c495152d65311
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-09-11 13:15:34 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -4,8 +4,8 @@ class Hash
   def keys_of(*arguments)
     keys_array = []
     arguments.each do |argument|
+      binding.pry
       if self.keys.include?(argument)
-        binding.pry
         keys_array << argument
       end
     end
